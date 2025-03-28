0a1e5197feab50ae6f1d511e2c0ac9a204ebb1ce
sb-bipin <bipinpoudel406@gmail.com>
2025-03-28 23:07:58 +0545

    Add project files.

---
+++ Domain/Entities/CustomerOrder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class CustomerOrder 
+    {
+        public Guid Id { get; set; }
+        public string CustomerName { get; set; }
+        public string TableNumber { get; set; }
+
+    }
+}
